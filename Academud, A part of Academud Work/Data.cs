fe6a8db93bcd26d354d72fd57819bc67e8dc82e7
RIProG <riprog@hotmail.com>
2018-12-12 16:47:46 +0100

    Första utkast av Classroom1 och Classroom2

--- Academud, A part of Academud Work/Data.cs
+++ Academud, A part of Academud Work/Data.cs
@@ -6,11 +6,13 @@ namespace Academud__A_part_of_Academud_Work
 {
     public class Data
     {
-        public string Playername { get; set; }
-        public int Warmth { get; set; }
-        public int Karma { get; set; }
-        public string Room { get; set; }
+        static public string Playername { get; set; }
+        static public int Warmth { get; set; }
+        static public int Karma { get; set; }
+        static public string Room { get; set; }
+        static public string ClassMate { get; set; }
 
+        static public string FavoritColor { get; set; }
 
 
     }
